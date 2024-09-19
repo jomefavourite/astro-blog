ac4b0d9805169788a7666f8510be41c80080de9f
Akos Komuves <akoskm@gmail.com>
2024-09-19 21:59:33 +0200

    add countdown timer

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const PROMOTION_END = new Date("2024-10-15T23:59:59Z").getTime(); // Set your promotion end date here
+let timeLeft = PROMOTION_END - Date.now();
+
+const formatTime = (time: number) => {
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((time % (1000 * 60)) / 1000);
+  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+};
+
+const Countdown = () => {
+  const [time, setTime] = useState(formatTime(timeLeft));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      timeLeft = PROMOTION_END - Date.now();
+      setTime(formatTime(timeLeft));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return <span id="countdown">Time left: {time}</span>;
+};
+
+export default Countdown;
